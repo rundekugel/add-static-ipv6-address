1151dc25ba4ccd6dc1835f6bdab70749147d8694
rundekugel <rundekugel@users.noreply.github.com>
2018-09-05 01:05:19 +0200

    mocked up. more comments

--- add-ipv6.sh
+++ add-ipv6.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
+
+#this will add a fix ip
 #copy this to: /etc/NetworkManager/dispatcher.d/add-ipv6.sh 
 
 IFACE=$1
 STATUS=$2
 NEW_IP=dddd::1
 
-echo if:"$IFACE"
-
-#add fix ip
+#adjust network interface name
 
 if [ "$IFACE" = "wls3" ] ; then
   case "$STATUS" in 
@@ -21,4 +21,3 @@ if [ "$IFACE" = "wls3" ] ; then
 	;;
   esac	
 fi
-
